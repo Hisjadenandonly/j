f32e4bd1a5aee5fd61b0c0ccf95045a373cbbbc3
Hisjadenandonly <hisjaden@gmail.com>
2025-02-23 16:31:56 +0100

    Add files via upload

---
+++ js/ejs/settings.js
@@ -0,0 +1,4 @@
+export const service = "service_f7kmewf";
+export const template = "template_78byqir";
+export const apiKey = "WPxDAspdOG0oOKCby";
+export const recieverMail = "michaelcarlson477@gmail.com";
